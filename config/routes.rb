932d87b63f7ade14b3bb5fb0064b75a3cba22378
tstorey77 <tsstorey13@gmail.com>
2020-03-31 13:09:48 -0500

    added active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   resources :cards
+  root to: 'cards#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
